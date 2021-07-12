c85650b68056ee3c09aae449afe55d9d19ce4cc0
asa-davis <13036228+asa-davis@users.noreply.github.com>
2021-07-12 11:56:26 -0600

    Update server.js

--- trainingportal/server.js
+++ trainingportal/server.js
@@ -573,7 +573,7 @@ process.on('SIGINT', function() {
   process.exit();
 });
 
-app.listen(8081,function(){
+app.listen(8081, '0.0.0.0', function(){
     util.log('Listening on 8081');
     util.log('Configured url:'+config.dojoUrl);
     util.log('Is secure:'+config.dojoUrl.startsWith("https")); 
